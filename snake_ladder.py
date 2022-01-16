b5afed8f20c14b45647ff35a5ca3271a019b9a63
rkverma2795 <85684822+rkverma2795@users.noreply.github.com>
2022-01-17 00:33:31 +0530

    Update snake_ladder.py
    dynamic input

--- snake_ladder.py
+++ snake_ladder.py
@@ -1,14 +1,7 @@
 import random
 
-Snake_bite = {
-    14:4,
-    20:2,
-    36:16,
-    96:12
-}
-
 # Player class
-class Player:
+class Player():
     def __init__(self, inPlayerNum):
         self.playerPos = 2
         self.playerNum = inPlayerNum
@@ -21,13 +14,13 @@ class Player:
 
     def getPlayerNum(self):
         return self.playerNum
-
+            
 
 # Function to handle the players turn
 def gameMaster(inPlayer):
     global winner
     # check for game winner
-    if inPlayer.getPosition() == 100:
+    if inPlayer.getPosition() == size_of_board:
             print("Player %i is the Winner!" % inPlayer.getPlayerNum())
             winner = True
 
@@ -37,24 +30,42 @@ def gameMaster(inPlayer):
         # Uncomment to require space to be pressed before jumping turns
         # input()
         roll = rollDice()
-        print("You rolled: %i" % roll)
+        print("abcd rolled: %i" % roll)
         movePlayer(inPlayer, roll)
         checkPosition(inPlayer)
 
 
 # Function to return the Integer value of a 6 side dice roll
 def rollDice():
+    """
+    even number on dice
+    """
     result = random.choice((2, 4, 6))
     return result
 
 
+def crooked_dice():
+    user_input = input("Do you want to enter snake moves(Y/N): ")
+    if user_input.lower() == 'yes' or 'Y' or 'y':
+        Snake_bite = {}
+        no_of_move = int(input('Enter Number of moves :'))
+        print("Enter {} move".format(no_of_move))
+        for i in range(no_of_move):
+            key, value = input().split()
+            Snake_bite[key] = value
+
+    else:
+        Snake_bite = {}
+    Snake_bite = "{"+", ".join(["{}:{}".format(k,v) for k,v in Snake_bite.items()])+"}"
+    return  Snake_bite
+    
 # Handle player movements
 def movePlayer(inPlayer, roll):
-    if inPlayer.getPosition() + roll <= 100:
+    if inPlayer.getPosition() + roll <= size_of_board:
         inPlayer.updatePosition(inPlayer.getPosition() + roll)
-        print("You are at spot %i" % inPlayer.getPosition())
+        print("abcd at spot %i" % inPlayer.getPosition())
     else:
-        print("You rolled too far")
+        print("abcd rolled too far")
 
 
 # Checks player landing position and adjusts if snake or ladder
@@ -72,8 +83,11 @@ def checkPosition(inPlayer):
 if __name__ == '__main__':
     global winner
     winner = False
+    size_of_board = int(input("Enter the size of board: "))
     numPlayers = int(input('Enter number of players: '))
-    Player_name = input("Enter the player name:")
+    Player_name = input("Enter the player name 01:")
+    Snake_bite = crooked_dice()
+    print(Snake_bite)
     playerList = []
     
     for i in range(0,numPlayers):
@@ -83,45 +97,3 @@ if __name__ == '__main__':
         for i in playerList:
             if winner == False:
                 gameMaster(i)
-
-
-
-
-
-# import random
-# def roll_dice():
-#     result = random.choice(2, 4, 6)
-#     return result
-# board_size = int(input("enter size of the board: "))
-# no_of_players = int(input("enter number of players wants to join: "))
-# player_name = input("enter player name of 01: ")
-# print("Welcome {} ".format(player_name))
-# bite_positions = {
-#     10: 1,
-#     14: 4,
-#     }
-# current_position = 0
-# decision = "y"
-# while (decision == "y"):
-#     decision = input("press y to continue OR n to quit the game: ")
-#     if decision == "n":
-#         break
-#     current_position += roll_dice()
-#     if current_position in bite_positions:
-
-
-# Snakes and Ladders dictionary
-# SaLdic = {6: 17,
-#           14: 3,
-#           20: 15,
-#           24: 26,
-#           30: 44,
-#           39: 33,
-#           49: 62,
-#           66: 53,
-#           69: 58,
-#           79: 67,
-#           82: 86,
-#           84: 71,
-#           88: 36,
-#           }
